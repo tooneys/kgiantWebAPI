3ffbc8d95c841ae942bae39db238d69c1c0430be
Tooney <Tooney@DESKTOP-TRO4RFJ>
2023-10-10 11:06:56 +0900

    update 2023-10-10

---
+++ kgiantWebAPI/Controllers/NotificationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace kgiantWebAPI.Controllers
+{
+    public class NotificationController : Controller
+    {
+        public IActionResult SendNotification(String data)
+        {
+
+            return View();
+    }
+}
+}
